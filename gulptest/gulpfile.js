e883b088b987aaafb9c7dd6d6cdd786e068191df
罗浩然 <2674669600@qq.com>
2019-04-19 23:30:54 +0800

    gulp压缩页面

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp'),
-    uglify = require("gulp-uglify"),//压缩js
-    babel = require("gulp-babel"),//es6转5
-    merge=require("merge-stream");
-    es2015Preset = require("babel-preset-es2015"),//es6转5
-    cssUglify = require('gulp-minify-css'),//压缩css
-    concat = require('gulp-concat'),   //合并文件
-    rename = require('gulp-rename'),   //文件重命名
-    jshint = require('gulp-jshint'),   //js检查
-    notify = require('gulp-notify'),   //提示
-    imageMin = require('gulp-imagemin'),//图片压缩
-    htmlmin = require('gulp-htmlmin'),//html压缩
-    gulpRemoveHtml = require('gulp-remove-html'),//标签清除，参考：https://www.npmjs.com/package/gulp-remove-html
-    removeEmptyLines = require('gulp-remove-empty-lines');//清除空白行，参考：https://www.npmjs.com/package/gulp-remove-empty-lines
+      uglify = require("gulp-uglify"),//压缩js
+      babel = require("gulp-babel"),//es6转5
+      merge = require("merge-stream");
+      es2015Preset = require("babel-preset-es2015"),//es6转5
+      cssUglify = require('gulp-minify-css'),//压缩css
+      concat = require('gulp-concat'),   //合并文件
+      rename = require('gulp-rename'),   //文件重命名
+      jshint = require('gulp-jshint'),   //js检查
+      notify = require('gulp-notify'),   //提示
+      imageMin = require('gulp-imagemin'),//图片压缩
+      htmlmin = require('gulp-htmlmin'),//html压缩
+      gulpRemoveHtml = require('gulp-remove-html'),//标签清除，参考：https://www.npmjs.com/package/gulp-remove-html
+      removeEmptyLines = require('gulp-remove-empty-lines');//清除空白行，参考：https://www.npmjs.com/package/gulp-remove-empty-lines
 
 gulp.task('default', async () => {//默认执行
     gulp.start('js');
@@ -19,29 +19,41 @@ gulp.task('default', async () => {//默认执行
 
 
 gulp.task("js", async () => {//这是压缩但不合并  多个文件夹的js同时压缩
-   var js=gulp.src("js/*.js")
-        // .pipe(jshint())
-        // .pipe(jshint.reporter('default'))
+    var js = gulp.src("js/*.js")
+    // .pipe(jshint())//js语法检测
+    // .pipe(jshint.reporter('default'))
         .pipe(babel({presets: [es2015Preset]}))
-        .pipe(uglify())
+        .pipe(uglify(
+            {
+                mangle: {except: ['require', 'exports', 'module', '$']},//类型：Boolean 默认：true 是否修改变量名
+                compress: true,//类型：Boolean 默认：true 是否完全压缩
+                preserveComments: 'false' //保留所有注释
+            }
+        ))
         .pipe(gulp.dest("dist"))
         .pipe(notify({message: "js task ok"}));
 
-   var lib=gulp.src("js/lib/*.js")
-   // .pipe(jshint())
-   // .pipe(jshint.reporter('default'))
-       .pipe(babel({presets: [es2015Preset]}))
-       .pipe(uglify())
-       .pipe(gulp.dest("dist/lib"))
-       .pipe(notify({message: "lib task ok"}));
-   return merge(js,lib)
+    var lib = gulp.src("js/lib/*.js")
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+        .pipe(babel({presets: [es2015Preset]}))
+        .pipe(uglify(
+            {
+                mangle: {except: ['require', 'exports', 'module', '$']},//类型：Boolean 默认：true 是否修改变量名
+                compress: true,//类型：Boolean 默认：true 是否完全压缩
+                preserveComments: 'false' //保留所有注释
+            }
+        ))
+        .pipe(gulp.dest("dist/lib"))
+        .pipe(notify({message: "lib task ok"}));
+    return merge(js, lib)
 });
 
 gulp.task('css', async () => {
     return gulp.src('css/*.css')      //设置css
     // .pipe(concat('order_query.css'))      //合并css文件到"order_query"
     //     .pipe(gulp.dest('dist/css'))           //设置输出路径
-        // .pipe(rename({suffix: '.min'}))         //修改文件名
+    // .pipe(rename({suffix: '.min'}))         //修改文件名
         .pipe(cssUglify())                    //压缩文件
         .pipe(gulp.dest('dist'))            //输出文件目录
         .pipe(notify({message: 'css task ok'}));   //提示成功
@@ -60,22 +72,22 @@ gulp.task('css', async () => {
 // });
 
 
-// gulp.task('css', async () => {
-//     return gulp.src('public/assets/css/*.css')      //设置css
-//     // .pipe(concat('order_query.css'))      //合并css文件到"order_query"
-//         .pipe(gulp.dest('dist/css'))           //设置输出路径
-//         // .pipe(rename({suffix: '.min'}))         //修改文件名
-//         .pipe(cssUglify())                    //压缩文件
-//         .pipe(gulp.dest('dist/assets/css'))            //输出文件目录
-//         .pipe(notify({message: 'css task ok'}));   //提示成功
-// });
+gulp.task('css', async () => {
+    return gulp.src('public/assets/css/*.css')      //设置css
+    // .pipe(concat('order_query.css'))      //合并css文件到"order_query"
+        .pipe(gulp.dest('dist/css'))           //设置输出路径
+        // .pipe(rename({suffix: '.min'}))         //修改文件名
+        .pipe(cssUglify())                    //压缩文件
+        .pipe(gulp.dest('dist/assets/css'))            //输出文件目录
+        .pipe(notify({message: 'css task ok'}));   //提示成功
+});
 
 
-// gulp.task('image', async () => {//压缩图片
-//     gulp.src('public/images/*.*')
-//         .pipe(imageMin({progressive: true}))
-//         .pipe(gulp.dest('dist/images'))
-// });
+gulp.task('image', async () => {//压缩图片
+    gulp.src('public/images/*.*')
+        .pipe(imageMin({progressive: true}))
+        .pipe(gulp.dest('dist/images'))
+});
 
 
 gulp.task('html', function () {//压缩html
